c932ff01ec46f15499b8ac217de14c562591ecbb
Daniel Martinez <dmartinez@itexico.net>
2017-02-07 14:29:49 -0600

    Resolve problem of parallelism with a arrayList using collector

--- src/org/sample/streams/ParrallelStreams.java
+++ src/org/sample/streams/ParrallelStreams.java
@@ -2,6 +2,7 @@ package org.sample.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ParrallelStreams {
@@ -12,13 +13,15 @@ public class ParrallelStreams {
 		
 		List<String> strings = new ArrayList<>();
 		
-		Stream.iterate("+", s -> s + "+")
-			//.parallel()
-			.limit(1000)
-			//.peek(s -> System.out.println(s + " processed in the thread " + Thread.currentThread().getName()))
-			.forEach(s -> strings.add(s));
+		List<String> collect = 
+				Stream.iterate("+", s -> s + "+")
+					.parallel()
+					.limit(1000)
+					//.peek(s -> System.out.println(s + " processed in the thread " + Thread.currentThread().getName()))
+					//.forEach(s -> strings.add(s));
+					.collect(Collectors.toList());
 		
-		System.out.println("# " + strings.size());
+		System.out.println("# " + collect.size());
 	}
 
 }
