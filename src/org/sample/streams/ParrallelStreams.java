4905dee94d8460d3541f8ce297219c2e4707d13e
Daniel Martinez <dmartinez@itexico.net>
2017-02-07 14:20:26 -0600

    Write example that print + symbols , add parallel and limit the quantity of thread pool

--- src/org/sample/streams/ParrallelStreams.java
+++ src/org/sample/streams/ParrallelStreams.java
@@ -1,8 +1,18 @@
 package org.sample.streams;
 
+import java.util.stream.Stream;
+
 public class ParrallelStreams {
 	
 	public static void main(String[] args) {
+		
+		System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "2");
+		
+		Stream.iterate("+", s -> s + "+")
+			.parallel()
+			.limit(6)
+			.peek(s -> System.out.println(s + " processed in the thread " + Thread.currentThread().getName()))
+			.forEach(System.out::println);
 		
 	}
 
